a9a3872c6231b384b75c82faec34bdc5517be718
whistleJs <whistle_js@naver.com>
2020-07-07 20:11:55 +0900

    Form Component 생성

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Form.css';
+
+const Form = ({ value, onChange, onCreate, onKeyPress }) => {
+  return (
+    <div>
+      <div className="form">
+        <input value={value} onChange={onChange} onKeyPress={onKeyPress} />
+
+        <div className="create-button" onClick={onCreate}>
+          추가
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Form;
