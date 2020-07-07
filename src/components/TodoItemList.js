ae824b7df64eea8d161b3d8f204f0c715c258991
whistleJs <whistle_js@naver.com>
2020-07-07 20:29:33 +0900

    TodoItemList - TodoItem 추가

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import TodoItem from './TodoItem';
+
 class TodoItemList extends Component {
   render() {
     const { todos, onToggle, onRemove } = this.props;
 
     return (
       <div>
+        <TodoItem text="Hello" />
+        <TodoItem text="React" />
+        <TodoItem text="Javascript" />
       </div>
     )
   }
