1384551380ae16f5badaf2b06e4ab3338b923857
whistleJs <whistle_js@naver.com>
2020-07-07 20:29:44 +0900

    App.js - TodoItemList 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
-import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
+import TodoListTemplate from './components/TodoListTemplate';
+import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
   render() {
     return (
       <TodoListTemplate form={<Form />}>
-        템플릿 완성
+        <TodoItemList />
       </TodoListTemplate>
     )
   }
